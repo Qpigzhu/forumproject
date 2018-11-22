006e33a0ccae37ebd7d6dfe03539e8b25d995478
Pig <331103418@qq.com>
2018-11-22 20:25:56 +0800

    模板的修改

--- forum/forum/views.py
+++ forum/forum/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import render
-from userforum.models import Forum,SowingMap
+from userforum.models import Forum,SowingMap,ForumType
 # Create your views here.
 
 
 def index(request):
     all_forum = Forum.objects.all()
     all_sowing_map = SowingMap.objects.all()
+    all_forum_type = ForumType.objects.all()
     return render(request, 'index.html', {
         'all_forum': all_forum,
         'all_sowing_map':all_sowing_map,
+        'all_forum_type':all_forum_type,
     })
