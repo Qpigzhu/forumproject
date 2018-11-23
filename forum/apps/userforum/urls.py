5aa934bd4f4972c00eb1b9b9a72de16ec8ef610d
Pig <331103418@qq.com>
2018-11-23 12:30:37 +0800

    详情页面添加

--- forum/apps/userforum/urls.py
+++ forum/apps/userforum/urls.py
@@ -3,8 +3,9 @@ __author__ = 'pig'
 __data__ = '2018\11\22 0022 10:30$'
 
 from django.urls import path
-from .views import ForumView
+from .views import ForumDatileView
 
 urlpatterns = [
-    path('',ForumView.as_view(),name="forum_datil"),
+
+    path('<int:forum_id>',ForumDatileView.as_view(),name="forum_datil"),
 ]
--- forum/apps/userforum/views.py
+++ forum/apps/userforum/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views import View
 from .models import Forum
 # Create your views here.
 
-class ForumView(View):
-    def get(self,request):
-        return render(request,'info.html',{
+#
+# class ForumView(View):
+#     def get(self,request):
+#
+#         return render(request,'info.html',{
+#
+#         })
 
+#详情页面
+class ForumDatileView(View):
+    def get(self, request,forum_id):
+        forum = get_object_or_404(Forum,id=forum_id)
+        return render(request, 'info.html', {
+            "forum":forum,
         })
-
